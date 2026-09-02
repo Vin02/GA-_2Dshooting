b7f18d7e6ead231c1077dce9a1648d944ba6340d
Vin Lee <jacoblee02@naver.com>
2026-09-02 15:00:26 +0900

    fix(Player) : 코드 규약에 맞게 재작성

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -4,18 +4,18 @@ public class PlayerFire : MonoBehaviour
 {
     // 스페이스바를 누를 때 마다 총알을 생성해서 발사하고 싶음
     // 필요 속성 : 총알 프리팹/생성 위치(총구)
-    public GameObject BulletPrefab;
-    public Transform LeftFirePoint;
-    public Transform RightFirePoint;
+    public GameObject bulletPrefab;
+    public Transform leftFirePoint;
+    public Transform rightFirePoint;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject Leftbullet = Instantiate(BulletPrefab);
-            Leftbullet.transform.position = LeftFirePoint.transform.position;
+            GameObject leftbullet = Instantiate(bulletPrefab);
+            leftbullet.transform.position = leftFirePoint.transform.position;
             
-            GameObject Rightbullet = Instantiate(BulletPrefab);
-            Rightbullet.transform.position = RightFirePoint.transform.position;
+            GameObject rightbullet = Instantiate(bulletPrefab);
+            rightbullet.transform.position = rightFirePoint.transform.position;
         }
         
     }
