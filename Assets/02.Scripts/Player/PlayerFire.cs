d79a9780fccd39e28895f621e0ba784988fcb190
Vin Lee <jacoblee02@naver.com>
2026-09-02 14:26:31 +0900

    feat(Player) : 총알 색상 변경

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,12 +5,13 @@ public class PlayerFire : MonoBehaviour
     // 스페이스바를 누를 때 마다 총알을 생성해서 발사하고 싶음
     // 필요 속성 : 총알 프리팹/생성 위치(총구)
     public GameObject BulletPrefab;
+    public Transform FirePoint;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = this.transform.position;
+            bullet.transform.position = FirePoint.transform.position;
         }
         
     }
--- Assets/03.Prefabs/Bullet.cs
+++ Assets/03.Prefabs/Bullet.cs
@@ -2,10 +2,35 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float Speed = 3f;
 
-    // 총알을 위로 움직이게 하기
+    static int colorIndex = 0;
 
-    public float Speed;
+    Color[] colors =
+    {
+        Color.red,
+        new Color(1f, 0.5f, 0f),  
+        Color.yellow,
+        Color.green,
+        Color.blue,
+        new Color(0.2f, 0f, 0.5f), 
+        new Color(0.5f, 0f, 1f)    
+    };
+
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.color = colors[colorIndex];
+
+        colorIndex++;
+
+        if (colorIndex >= colors.Length)
+        {
+            colorIndex = 0;
+        }
+    }
+
     void Update()
     {
         Vector2 direction = Vector2.up;
