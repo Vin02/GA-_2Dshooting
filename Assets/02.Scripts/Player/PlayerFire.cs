93e7f2924803f1d4ed4bf4d37417e1fb4e8e9701
Vin Lee <jacoblee02@naver.com>
2026-09-02 14:55:58 +0900

    fear(Player) : 총구 2개 확장

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,13 +5,17 @@ public class PlayerFire : MonoBehaviour
     // 스페이스바를 누를 때 마다 총알을 생성해서 발사하고 싶음
     // 필요 속성 : 총알 프리팹/생성 위치(총구)
     public GameObject BulletPrefab;
-    public Transform FirePoint;
+    public Transform LeftFirePoint;
+    public Transform RightFirePoint;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = FirePoint.transform.position;
+            GameObject Leftbullet = Instantiate(BulletPrefab);
+            Leftbullet.transform.position = LeftFirePoint.transform.position;
+            
+            GameObject Rightbullet = Instantiate(BulletPrefab);
+            Rightbullet.transform.position = RightFirePoint.transform.position;
         }
         
     }
